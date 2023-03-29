01d69da0c74d431233d01f9753576c1073913df1
Bezwada Satyapravin <56443544+satyapravin@users.noreply.github.com>
2023-03-29 17:04:13 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'sample'
-copyright = u'2012, Kenneth Reitz'
+project = u'mm_tester'
+copyright = u'2023, Pravin Bezwada'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'sample', u'sample Documentation',
-     [u'Kenneth Reitz'], 1)
+    ('index', 'mm_tester', u'MM Testing Documentation',
+     [u'Pravin Bezwada'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'sample', u'sample Documentation',
-   u'Kenneth Reitz', 'sample', 'One line description of project.',
+  ('index', 'mm_tester', u'MM Testing Documentation',
+   u'Pravin Bezwada', 'mm_tester', 'Backtesting software for market making.',
    'Miscellaneous'),
 ]
 
